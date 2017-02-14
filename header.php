ae0775997f7d032f54b847c6752478fc1e37ca02
maustin17 <martintaustin@hotmail.com>
2017-02-13 21:01:09 -0600

    Add files via upload
    these should be what we worked on tonight

---
+++ header.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Austin
+ * Date: 1/16/2017
+ * Time: 7:20 PM
+ */
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Online Forms</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav id="myNavbar" class="navbar navbar-default" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.html">Home</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" data-toggle="dropdown" class="dropdown-toggle">Child Care Form<b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="http://firstuniv.weebly.com/childcare-reservation-form.html">Child Care Form</a></li>
+
+                    </ul>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" data-toggle="dropdown" class="dropdown-toggle">Volunteering Form<b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="form2.php">Volunteer Sign up</a></li>
+
+                    </ul>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" data-toggle="dropdown" class="dropdown-toggle">Space Request Form<b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="http://firstuniv.weebly.com/space-request-form.html">Space Request Form</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                    <a href="#" data-toggle="dropdown" class="dropdown-toggle">more...<b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="http://firstuniv.weebly.com/habitat-for-humanity-sign-up.html">Habitat for Humanity</a></li>
+                        <li><a href="http://firstuniv.weebly.com/content-submission-form.html">Content Submision Form</a></li>
+                        <li><a href="http://firstuniv.weebly.com/contact-us.html">Contact Us</a></li>
+                        <li><a href="http://firstuniv.weebly.com/offering-plate.html">Offering Plate</a></li>
+
+                    </ul>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+        <h1><center><bold>FIRST UNIVERSALIST</bold></center></h1>
+        <h1><center><bold>ONLINE FORMS</bold></center></h1>
+    </div>
+
+    <!-- Here's all it takes to make this navigation bar. -->
+
+
+
+
+
+
+    <!-- <a>Habitat for Humanity Sign-Up</a>
+                                     <a>Content Submission Form</a>
+                                     <a>Offering Plate</a>
+                                    <a>Contact Us</a>-->
+
+
+            
+
+
+
